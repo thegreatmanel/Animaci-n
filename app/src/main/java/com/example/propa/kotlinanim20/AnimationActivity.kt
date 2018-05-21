7e0c6169eed1ad5fcb8da841ae78965b66ca08b7
Manel <propaner14@gmail.com>
2018-05-21 15:03:04 +0200

    First commit. Interfaz preparada e imagen provisional insertada

---
+++ app/src/main/java/com/example/propa/kotlinanim20/AnimationActivity.kt
@@ -0,0 +1,19 @@
+package com.example.propa.kotlinanim20
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_animation.*
+
+class AnimationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animation)
+
+
+
+    }
+
+}
