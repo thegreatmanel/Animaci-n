44315999966d3c7176bad2107d457007ec6783c5
Manel <propaner14@gmail.com>
2018-05-21 17:22:48 +0200

    LLamada a segundo activity con éxito

--- app/src/main/java/com/example/propa/kotlinanim20/MainActivity.kt
+++ app/src/main/java/com/example/propa/kotlinanim20/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.propa.kotlinanim20
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        setContentView(R.layout.fragment_main)
+
+start_button.setOnClickListener{
+        val salto = Intent(this, AnimationActivity::class.java)
+        salto.putExtra("key1", "valor1")
+        startActivity(salto)
+    }}
 }
